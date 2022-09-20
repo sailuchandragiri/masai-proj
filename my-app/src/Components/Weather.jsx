f718e85ebdfd7ea587fd9018352e7e7e26e5a7f8
=sailuchandragiri <sailuchandragiri2000@gmail.com>
2022-09-20 10:33:25 +0530

    files

--- my-app/src/Components/Weather.jsx
+++ my-app/src/Components/Weather.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import axios from "axios"
+import "./Weather.css";
 
 const Weather = () => {
     const [city,setCity]=useState({})
@@ -42,13 +43,12 @@ const Weather = () => {
 
 
   return (
-    <div>
+    <div className='layout'>
         <div>
              <h1>Daily Weather Report</h1>
-             <div>
-                 <input type="text" placeholder='City' onChange={handleChange} style={{width:250, textAlign:"center"}}/>
-                 <br />
-                 <br />
+             <div className='search_lay'>
+                 <input type="text" placeholder='City' className='search' onChange={handleChange} style={{width:250, textAlign:"center"}}/>
+                
                  <button onClick={handleClick} style={{width:150, borderRadius:5, fontWeight:"bold"}}>Submit</button>
              </div>
         </div>
@@ -59,7 +59,7 @@ const Weather = () => {
                         <h1><span style={{color:"blue"}} onClick={handleForecast}>Click Here </span>See the Next 7 Days ForeCast</h1>
                     </div>
                     <div style={{display:"flex", "gap":"50px",width:"55%", margin:"auto"}}>
-                        <div style={{border:"1px solid red", backgroundColor:"#ff0ed7", fontWeight:"bold", width:"33%",textAlign:"left", paddingLeft:50 , marginTop:30, borderRadius:10}}>
+                        <div style={{border:"1px solid #FFB6C1", backgroundColor:"#FFB6C1", fontWeight:"bold", width:"33%",textAlign:"left", paddingLeft:50 , marginTop:30, borderRadius:10}}>
                             <p>City : {weatherData.name}</p>
                             <p>Sunrise : {weatherData.sys?.sunrise}</p>
                             <p>Sunset : {weatherData.sys?.sunset}</p>
@@ -72,17 +72,17 @@ const Weather = () => {
                             <p>Clouds  : {weatherData.clouds?.all}</p>
                         </div>
                         <div>
-                            <iframe style={{border:"solid #ff0ed7", color:"#ff0ed7", fontWeight:"bold", textAlign:"left",paddingTop:5, marginTop:30, height:400,width:"150%", borderRadius:10}} src={`https://www.google.com/maps/embed/v1/place?q=${weatherData.name}&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8`} title="Map"></iframe>
+                            <iframe style={{border:"solid #FFB6C1", color:"#ff0ed7", fontWeight:"bold", textAlign:"left",paddingTop:5, marginTop:30, height:400,width:"150%", borderRadius:10}} src={`https://www.google.com/maps/embed/v1/place?q=${weatherData.name}&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8`} title="Map"></iframe>
                         </div>   
                     </div> 
                 </div>
             ):("")
         }
         
-        <div style={{"display":"flex", width:"80%",gap:10,margin:"auto", marginTop:50}}>
+        <div style={{"display":"flex", width:"80%",gap:10,margin:"auto", marginTop:50, marginBottom:50}}>
             {
                 foreCast.map((el)=>(
-                    <div style={{border:"1px solid red", borderRadius:10,backgroundColor:"#10a310", fontWeight:"bold"}}>
+                    <div style={{border:"1px solid red", borderRadius:10,backgroundColor:"#0AFFFF", fontWeight:"bold"}}>
                         <p>{Date(el.dt*1000)}</p>
                         <img src={`http://openweathermap.org/img/wn/${el.weather[0].icon}@2x.png`} alt="" />
                         <p>Day Temp : {el.temp.day.toFixed(0)}C</p>
